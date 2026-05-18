b26e44335019aa81fd92d023a09ff3c72c623e8d
Chanakya <chanakyadba92@gmail.com>
2026-05-18 15:40:14 +0530

    sed command

--- mongodb.sh
+++ mongodb.sh
@@ -1,5 +1,5 @@
 mv mongo.repo /etc/yum.repos.d/
 dnf install mongodb-org -y 
 systemctl enable mongod 
-systemctl start mongod 
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/momgod.conf
 systemctl restart mongod

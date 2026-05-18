83685deac2a0c7b74d72b3e2639f12db3e886b48
Chanakya <chanakyadba92@gmail.com>
2026-05-18 15:43:09 +0530

    correction in spelling mongo

--- mongodb.sh
+++ mongodb.sh
@@ -1,5 +1,5 @@
 mv mongo.repo /etc/yum.repos.d/
 dnf install mongodb-org -y 
 systemctl enable mongod 
-sed -i 's/127.0.0.1/0.0.0.0/' /etc/momgod.conf
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 systemctl restart mongod

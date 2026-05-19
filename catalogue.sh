b3998aacc1b1f8754993c29a46a65af7e012ff39
Chanakya <chanakyadba92@gmail.com>
2026-05-19 13:36:23 +0530

    catalogue.sh and catalogue service file added

---
+++ catalogue.sh
@@ -0,0 +1,15 @@
+dnf module disable nodejs -y
+dnf module enable nodejs:18 -y
+cp catalogue.service /etc/systemd/system/
+useradd roboshop
+mkdir /app 
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip 
+cd /app 
+unzip /tmp/catalogue.zip
+npm install 
+systemctl daemon-reload
+systemctl enable catalogue 
+systemctl start catalogue
+cp mongo.repo /etc/yum.repos.d/
+dnf install mongodb-org-shell -y
+mongo --host MONGODB-SERVER-IPADDRESS </app/schema/catalogue.js

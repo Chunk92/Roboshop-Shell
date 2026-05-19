4c15422ae2c5c4dbdc628235c2be40e4a1dc8e70
Chanakya <chanakyadba92@gmail.com>
2026-05-19 15:57:40 +0530

    added / in sed redis.sh

--- redis.sh
+++ redis.sh
@@ -1,7 +1,7 @@
 dnf module disable redis -y
 dnf module enable redis:6 -y
 dnf install redis -y 
-sed -i 's/127.0.0.1/0.0.0.0' /etc/redis.conf
-sed -i 's/127.0.0.1/0.0.0.0' /etc/redis/redis.conf
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf
 systemctl enable redis 
 systemctl start redis 
